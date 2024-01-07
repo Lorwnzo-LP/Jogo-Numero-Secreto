8c4750e5bb27b099705e786c346c94f93f0dd764
Lorenzo Loss <lorenzocontato1@outlook.com>
2024-01-06 21:10:25 -0300

    alterando 50 numeroLimite

--- Pratica/app.js
+++ Pratica/app.js
@@ -1,7 +1,7 @@
 // Jogo do Número Secreto
 let listaNumerosSorteados = [];
 let tentativas = 1;
-let numeroLimite = 50;
+let numeroLimite = 100;
 
 exibirMensagemComeço()
 
@@ -12,7 +12,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemComeço(){
     exibirTextoNaTela("h1","Bem vindo ao jogo do número secreto!");
-    exibirTextoNaTela("p",`Escreva um número de 1 a 50:`);
+    exibirTextoNaTela("p",`Escreva um número de 1 a 100:`);
 }
 
 let numeroSecreto = gerarNumeroAleatorio();
